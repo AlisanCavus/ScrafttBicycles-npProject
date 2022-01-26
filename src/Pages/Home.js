77a90c6af2e43127f8f68b4988ec2ce6198181da
AlisanCavus <cavusalisan@gmail.com>
2022-01-26 22:46:28 +0100

    hh

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,15 +1,15 @@
 import React from 'react';
 import { Parallax, ParallaxLayer } from 'react-spring'
-import HomeText from '../Components/HomeComponents/HomeText';
+
 
 
 function Home() {
 
   
   return (
-    <div className='bg-bcycle bg-cover min-h-screen min-w-full opacity-50 -mt-[5rem]'>
-      <HomeText/>
-    </div>
+    <div className=" min-h-screen bg-primary">
+
+ </div>
     
   
     
--- src/Pages/Products.js
+++ src/Pages/Products.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 
 function Products() {
-  return <div className=" h-screen bg-primary">
+  return <div className=" min-h-screen bg-primary">
     <div className='absolute'>Products</div>
   </div>;
 }
