7e1b75d9c102353766f9681b569a2fdce351d99d
AlisanCavus <cavusalisan@gmail.com>
2022-01-28 14:16:00 +0100

    scroll to top

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,8 @@ import React from 'react';
 import FirstJumb from '../Components/HomeComps/FirstJumb';
 import HomeProd from '../Components/HomeComps/HomeProd';
 import Prod1 from '../Components/HomeComps/Prod1';
-import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll'
+import * as Scroll from 'react-scroll';
+
 
 
 
@@ -10,21 +11,23 @@ import { Link, Button, Element, Events, animateScroll as scroll, scrollSpy, scro
 
 function Home() {
 
-  const scrollMore = function() {
-    scroll.scrollTo(100);
-  }
+
+  
+ 
 
   
   return (
-    <div className=" min-h-screen bg-primary overflow-x-hidden scroll-smooth snap-y  snap-always" onWheel={scrollMore} >
-        <div className="bg-bicycle bg-cover min-h-screen opacity-60">
+    <div className=" min-h-screen bg-primary overflow-x-hidden"   >
+      <div>
+        <div className="bg-bicycle bg-cover min-h-screen opacity-60 ">
             <FirstJumb />
         </div>
-        <div className=" min-h-screen mx-auto  bg-primary  z-30 mobile:w-full overflow-hidden">
+        <div className=" min-h-screen mx-auto  bg-primary  z-30 mobile:w-full overflow-hidden ">
           <HomeProd />
         </div>
-        <div className="min-h-screen bg-primary overflow-hidden">
+        <div className="min-h-screen bg-primary overflow-hidden ">
           <Prod1 />
+        </div>
         </div>
     </div>
     
