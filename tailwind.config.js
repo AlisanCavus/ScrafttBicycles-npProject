7070905e8b0f7abea3cc79b7bd4b983fb4b26b24
AlisanCavus <cavusalisan@gmail.com>
2022-02-01 20:46:43 +0100

    ggg

--- tailwind.config.js
+++ tailwind.config.js
@@ -42,6 +42,15 @@ module.exports = {
         'bicycle' : "url('https://images.pexels.com/photos/686230/pexels-photo-686230.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260') ",
         'tamir' : "url('https://images.pexels.com/photos/5446308/pexels-photo-5446308.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')",
         'garage' : "url('https://images.pexels.com/photos/6156531/pexels-photo-6156531.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260')"
+      },
+      keyframes: {
+        wiggle: {
+          '0%, 100%': { transform: 'rotate(-3deg)' },
+          '50%': { transform: 'rotate(3deg)' },
+        }
+      },
+      animation: {
+        wiggle: 'wiggle 1s ease-in-out infinite',
       }
     },
   },
