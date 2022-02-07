53f9de000174b94c14cc9a0353cde7c27dc9b5a7
AlisanCavus <cavusalisan@gmail.com>
2022-02-07 17:30:56 +0100

    gkal

--- src/Pages/Favorites.js
+++ src/Pages/Favorites.js
@@ -1,10 +1,83 @@
-import React from 'react';
+import { getDocs } from '@firebase/firestore';
+import React, {useEffect, useState} from 'react';
+import { collref } from '../firebase';
+import LoadingScreen from '../components/LoadingScreen';
 
 
 function Favorites() {
-  return <div className=" h-screen bg-primary">
-    Favorites
-  </div>;
+  const [favBikes, setFavBikes] = useState([])
+  const [bikes, setBikes] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  const [ favoritedBikes, setFavoritedBikes ] = useState([])
+
+  useEffect(() => {
+    const data = localStorage.getItem('FavoriteBikes')
+    if (data) {
+      setFavBikes(JSON.parse(data));
+    }
+
+  },[]);
+
+
+  useEffect(() => {
+    const getbikes = () => {
+
+       getDocs(collref)
+        .then((snapshot) => {
+          let bicycles = [];
+          snapshot.docs.forEach((doc) => {
+            bicycles.push({ ...doc.data(), id: doc.id });
+          });
+          setBikes(bicycles);
+          setLoading(false)
+        
+        })
+        .catch((err) => {
+          console.log(err.message);
+        });
+    }
+
+    let isApiSubscribed = true
+    if (isApiSubscribed) {
+      getbikes()
+    }
+    
+  
+    return () => {
+      // cancel the subscription
+      isApiSubscribed = false
+  };
+   
+   // eslint-disable-next-line react-hooks/exhaustive-deps
+   },[]);
+
+
+
+useEffect(()=> {
+  setLoading(true)
+  setFavoritedBikes(bikes.filter((_, i)=> favBikes[i]))
+  setLoading(false)
+
+},[bikes, favBikes])
+
+console.log(favoritedBikes)
+
+  if ( loading ) {  
+    return(<LoadingScreen/>) 
+  } else {
+    return (<div className=" min-h-screen bg-primary">
+      <ul className="h-11/12 w-11/12 rounded border-2 flex justify-center">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+    )}
+ 
+  
+  
 }
 
 export default Favorites;
--- src/components/ProductsCard.js
+++ src/components/ProductsCard.js
@@ -76,7 +76,7 @@ function ProductsCard(props) {
     prevArrow: <PrevArrow />,
   };
 
-  if (index % 2 === 0) {
+  if (index % 2 === 0 ) {
     return (
       <>
         <li
@@ -141,13 +141,13 @@ function ProductsCard(props) {
               <div className="h-2/12 text-slate-600 w-10/12 mx-auto my-2  mobile:text-sm">
                 Price: {price}€
               </div>
-
+            
               <div 
                onClick={() =>addToFav(id)}
               className="h-3/12 text-slate-600 w-10/12 flex justify-center mx-auto  my-2  ">
                 <button className="flex text-center justify-center rounded fill-slate-600 py-2 w-1/2  px-10 mobile:py-0 mobile:px-2 text-primary mobile:w-full bg-slate-700 align-middle">
                   
-                Add to Favorites {favBikes.length === 0 ? (<></>) : (<span className="mx-1"> ({favBikes.length}) </span>)}
+                Add to Favorites {favBikes.length === 0 ? (<></>) : (<span className="mx-1 "> ({favBikes.length}) </span>)}
                 </button>
               </div>
               {currentUser ? (
@@ -227,7 +227,7 @@ function ProductsCard(props) {
                 onClick={() =>addToFav(id)}
                 className="flex text-center justify-center rounded fill-slate-600 py-2 w-1/2  px-10 mobile:py-0 mobile:px-2 text-primary mobile:w-full bg-slate-700 align-middle">
                   
-                  Add to Favorites {favBikes.length === 0 ? (<></>) : (<span> ({favBikes.length}) </span>)}
+                  Add to Favorites {favBikes.length === 0 ? (<></>) : (<span className="mx-1 "> ({favBikes.length}) </span>)}
                 </button>
               </div>
 
