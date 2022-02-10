30b5a4f70ed746ed67ac6e5794f54d15978bffa7
AlisanCavus <cavusalisan@gmail.com>
2022-02-10 10:39:19 +0100

    add cart funct okay

--- src/components/ProductsCard.js
+++ src/components/ProductsCard.js
@@ -32,7 +32,7 @@ function ProductsCard(props) {
     specs,
     addToFav,
     favBikes,
-    addToCard,
+    addToCart,
     cartBikes,
    
    
@@ -151,7 +151,7 @@ function ProductsCard(props) {
                 </button>
               </div>
               {currentUser ? (
-                <div onClick={() => addToCard(id, model, price, brand, img0)} className="h-3/12 text-slate-600 w-10/12 flex justify-center mx-auto my-2">
+                <div onClick={() => addToCart(id, model, price, brand, img0)} className="h-3/12 text-slate-600 w-10/12 flex justify-center mx-auto my-2">
                   <button className="flex text-center rounded fill-slate-600  w-1/2  px-10 py-2 mobile:py-0 mobile:px-2 text-primary justify-center mobile:w-full bg-slate-700 align-middle ">
                     
                   Add to Card {cartBikes.length === 0 ? (<></>) : (<span className="mx-1 "> ({cartBikes.length}) </span>)}
@@ -234,10 +234,10 @@ function ProductsCard(props) {
               </div>
 
               {currentUser ? (
-                <div onClick={() => addToCard(id, model, price, brand, img0)} className="h-3/12 text-slate-600 w-10/12 flex justify-center mx-auto my-2">
+                <div onClick={() => addToCart(id, model, price, brand, img0)} className="h-3/12 text-slate-600 w-10/12 flex justify-center mx-auto my-2">
                   <button className="flex text-center rounded fill-slate-600  w-1/2  px-10 py-2 mobile:py-0 mobile:px-2 text-primary justify-center mobile:w-full bg-slate-700 align-middle ">
                     
-                  Add to Card {favBikes.length === 0 ? (<></>) : (<span className="mx-1 "> ({cartBikes.length}) </span>)}
+                  Add to Card {cartBikes.length === 0 ? (<></>) : (<span className="mx-1 "> ({cartBikes.length}) </span>)}
                   </button>
                 </div>
               ) : (
