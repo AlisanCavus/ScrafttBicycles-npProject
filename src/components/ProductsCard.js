6603fd94f3e16db195a5f94fbffee668ee5576bf
AlisanCavus <cavusalisan@gmail.com>
2022-02-10 19:57:24 +0100

    test

--- src/components/ProductsCard.js
+++ src/components/ProductsCard.js
@@ -154,7 +154,7 @@ function ProductsCard(props) {
                 <div onClick={() => addToCart(id, model, price, brand, img0)} className="h-3/12 text-slate-600 w-10/12 flex justify-center mx-auto my-2">
                   <button className="flex text-center rounded fill-slate-600  w-1/2  px-10 py-2 mobile:py-0 mobile:px-2 text-primary justify-center mobile:w-full bg-slate-700 align-middle ">
                     
-                  Add to Card {cartBikes.length === 0 ? (<></>) : (<span className="mx-1 "> ({cartBikes.length}) </span>)}
+                  Add to Cart {cartBikes.length === 0 ? (<></>) : (<span className="mx-1 "> ({cartBikes.length}) </span>)}
                   </button>
                 </div>
               ) : (
@@ -237,7 +237,7 @@ function ProductsCard(props) {
                 <div onClick={() => addToCart(id, model, price, brand, img0)} className="h-3/12 text-slate-600 w-10/12 flex justify-center mx-auto my-2">
                   <button className="flex text-center rounded fill-slate-600  w-1/2  px-10 py-2 mobile:py-0 mobile:px-2 text-primary justify-center mobile:w-full bg-slate-700 align-middle ">
                     
-                  Add to Card {cartBikes.length === 0 ? (<></>) : (<span className="mx-1 "> ({cartBikes.length}) </span>)}
+                  Add to Cart {cartBikes.length === 0 ? (<></>) : (<span className="mx-1 "> ({cartBikes.length}) </span>)}
                   </button>
                 </div>
               ) : (
